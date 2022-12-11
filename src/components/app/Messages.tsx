8cb20efd3dc6545ddf7a0111d9879058d698b5df
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2022-12-11 14:59:05 +0330

    Add toggle component

--- src/components/app/Messages.tsx
+++ src/components/app/Messages.tsx
@@ -5,6 +5,14 @@ const Messages = () => {
   return (
     <div className="flex flex-col gap-4">
       <Input />
+      <div className="relative  py-2 px-2 flex justify-between items-center gap-4 bg-gray-200/60 rounded-md">
+        <button className="h-9 w-full  font-semibold text-blue-500 shadow-sm bg-white rounded-md">
+          Direct
+        </button>
+        <button className="h-9 w-full  font-semibold text-gray-400">
+          Groups
+        </button>
+      </div>
     </div>
   );
 };
