345f6b18c8ad7b6968c60925fa433ae44980219b
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2022-12-11 15:09:28 +0330

    Add animation to Toggle

--- src/components/app/Messages.tsx
+++ src/components/app/Messages.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./ui/Input";
 
+type SwitchBtn = "Direct" | "Groups";
+
 const Messages = () => {
+  const [switchValue, setSwitchValue] = useState<SwitchBtn>("Direct");
+
+  const handleDirectBtn = () => {
+    setSwitchValue("Direct");
+  };
+  const handleGroupsBtn = () => {
+    setSwitchValue("Groups");
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <Input />
       <div className="relative  py-2 px-2 flex justify-between items-center gap-4 bg-gray-200/60 rounded-md">
-        <button className="h-9 w-full  font-semibold text-blue-500 shadow-sm bg-white rounded-md">
+        <button
+          onClick={handleDirectBtn}
+          className={`h-9 w-full  font-semibold transition-all duration-200 ${
+            switchValue === "Direct"
+              ? "text-blue-500 shadow-sm bg-white rounded-md"
+              : "text-gray-500"
+          }`}
+        >
           Direct
         </button>
-        <button className="h-9 w-full  font-semibold text-gray-400">
+        <button
+          onClick={handleGroupsBtn}
+          className={`h-9 w-full  font-semibold  transition-all duration-200 ${
+            switchValue === "Groups"
+              ? "text-blue-500 shadow-sm bg-white rounded-md"
+              : "text-gray-500"
+          }`}
+        >
           Groups
         </button>
       </div>
