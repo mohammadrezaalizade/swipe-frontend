106c27f4fb23eeabd1569742732f1a6c10262a2c
Mohammadreza Alizadeh <mohammadrezaalizadeh2002@gmail.com>
2022-12-13 22:55:39 +0330

    Add Contact components

--- src/components/app/ui/message/Contact.tsx
+++ src/components/app/ui/message/Contact.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import avatar from "../../../../assets/avatars/avatar-male-1.jpg";
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="flex gap-3 items-center">
+      <img
+        src={avatar}
+        alt="contact prrofile"
+        className="w-12 h-12 rounded-full"
+      />
+      <div className="flex flex-col">
+        <h1 className="text-sm font-semibold text-gray-600">Mohammad NRZ</h1>
+        <p className="text-xs text-gray-500 font-medium">online</p>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
